ddea56727afd7482a1e93309edae54c91c391d6a
reinerkp <kate.reiner1@gmail.com>
2023-02-04 12:25:30 -0700

    adding sutocode

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+// Bring in DAYJS 
+
+// Check out  data reurned from API 
+    // Requires Latitude and Long 
+
+    // Requires City name, returns Lat.Lon
+
+// Event Listener 
+    // Search Button 
+    // Each History Button 
+
+
+// Function!!
+// Write Fetch Call to API 
+    // Find the informaiton that we want to display from the API call response 
+        // Todays Date 
+        // 5 day forcast 
+
+// Render Function 
+// Programatically display that data to the page
+// for Loop 2 times 
+    // Create x Card 
+        // Add x.header
+        // Add x.image 
+        // Add x.temp 
+        // Add x.wind 
+        // Add x.humidity 
+        // Append x Card to some Div
+
+// Local Storage 
+    // When the page loads, grab all of the cities in LocalStorage 
+    // When a city name is input into the form and the search button in clicked, append the city to ecisting localStorage
