d90bb5df1c1a75dacf73ac1b1fc4a0a6ace4df55
reinerkp <kate.reiner1@gmail.com>
2023-02-08 20:15:44 -0700

    fixed forecast dispaly Javascript and created function to render forecast weather

--- Assets/script.js
+++ Assets/script.js
@@ -5,12 +5,14 @@ var apiKey = "c18058442237a091008225c9b4803f9c";
 const searchForm = document.querySelector(".inputGroup");
 var searchItem = document.querySelector("#searchItem");
 var currentWeatherEl = document.querySelector(".current");
+
 var lsSearches = [];
 
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log(searchItem.value);
-  lsSearches += searchItem.value;
+
+  lsSearches.push(searchItem.value);
   localStorage.setItem("Recent Searches", JSON.stringify(lsSearches));
 
   fetch(
@@ -20,8 +22,8 @@ searchForm.addEventListener("submit", function (e) {
       return response.json();
     })
     .then(function (data) {
-        console.log('FIRST RESPONSE', data);
-      getWeather(data.coord.lat, data.coord.lon, data.name, data.weather);
+      console.log("FIRST RESPONSE", data);
+      getWeather(data.coord.lat, data.coord.lon, data.name, data);
       //call the second api and hand it (data)
     })
     .catch();
@@ -39,69 +41,48 @@ function getWeather(latitude, longitude, cityName, currentWeatherData) {
     .then(function (forecastWeather) {
       renderCurrentWeather(cityName, forecastWeather, currentWeatherData);
     });
-  // fetch()
-};
+}
 
 function renderCurrentWeather(cityName, forecastWeather, currentWeatherData) {
-    console.log('current: ', currentWeatherData)
-    console.log('forecast: ', forecastWeather)
-    currentWeatherEl.innerHTML = `
+  var todayDate = dayjs().format("MM/DD/YYYY");
+  console.log("current: ", currentWeatherData);
+  console.log("forecast: ", forecastWeather.list);
+  var tempKtoF = Math.floor(
+    (currentWeatherData.main.temp - 273.15) * (9 / 5) + 32
+  );
+
+  currentWeatherEl.innerHTML = `
         <h3>${cityName}</h3>
                 <div>
-                    <h4>${new Date().toLocaleDateString()}</h4>
+                    <h4>${todayDate}</h4>
                     <p>icon</p>
                 </div>
 
-                <p>${currentWeatherData.main}</p>
-                <p>wind</p>
-                <p>huminity</p>
+                <p>Temperature: ${tempKtoF} °F</p>
+                <p>Wind Speed: ${currentWeatherData.wind.speed} MPH</p>
+                <p>Humidity: ${currentWeatherData.main.humidity}%</p>
     `;
 
-    renderForecastWeather(forecastWeather);
-};
+  renderForecastWeather(forecastWeather);
+}
 
 function renderForecastWeather(forecastWeather) {
-    /*var dayiteration = 0;
-    document.querySelectorAll('.card').forEach(function () {
-        dayiteration++
-        
-        var result = forecastWeather.list.find(item => item.dt_txt = dayjs().add(dayiteration, 'day').format('YYYY-MM-DD'));
-        console.log(result);
+  var elements = document.getElementsByClassName("card");
+  for (var i = 0; i < elements.length; i++) {
+    x = i * 8;
+    let date = dayjs()
+      .add(i + 1, "day")
+      .format("MM/DD/YYYY");
 
-        let date = dayjs().add(dayiteration, 'day').format('MM/DD/YYYY');
-        console.log(date);
-        $(this).innerHTML = `
-            <h3>TEST</h3>
-                    <div>
-                        <h4>TEST</h4>
-                        <p>icon</p>
-                    </div>
-
-                    <p>TEST</p>
-                    <p>wind</p>
-                    <p>huminity</p>
-        `;        
-    });*/
-
-    var elements = document.getElementsByClassName('card')
-    for (var i = 0; i <= elements.length; i++) {
-        let date = dayjs().add(i, 'day').format('MM/DD/YYYY');
-        var filterDate = dayjs().add(i, 'day').format('YYYY-MM-DD');
-        filterDate = filterDate.concat(' 12:00:00');
-
-        var result = forecastWeather.list.find(item => item.dt_txt == filterDate);
-        console.log(result);
-       
-        /*elements[i].innerHTML = `
-            <h3>${date}</h3>
-            <p>Icon</p>
-            <p>${result.main.temp} °F</p>
-            <p>${result.wind.speed} MPH</p>
-            <p>${result.main.humidity}</p>
-            `;*/
-    }
-    
-};
+    elements[i].innerHTML = `
+              <h3>${date}</h3>
+              <p>Icon</p>
+              <p>Temperature: ${forecastWeather.list[x].main.temp} °F</p>
+              <p>Wind Speed: ${forecastWeather.list[x].wind.speed} MPH</p>
+              <p>Humidity: ${forecastWeather.list[x].main.humidity}%</p>
+              `;
+  }
+}
 
 // Bring in DAYJS
 
