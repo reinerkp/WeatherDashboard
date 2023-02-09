76ffd070e28b1842b60fc410391a4909511e4323
reinerkp <kate.reiner1@gmail.com>
2023-02-08 20:34:28 -0700

    formated notes

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,12 @@
-// var city =$("#searchItem").val();
-// // Save API key
+// Save API key
 var apiKey = "c18058442237a091008225c9b4803f9c";
 
-const searchForm = document.querySelector(".inputGroup");
+var searchForm = document.querySelector(".inputGroup");
 var searchItem = document.querySelector("#searchItem");
 var currentWeatherEl = document.querySelector(".current");
 
 var lsSearches = [];
-
+// Event Listener
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log(searchItem.value);
@@ -15,9 +14,11 @@ searchForm.addEventListener("submit", function (e) {
   lsSearches.push(searchItem.value);
   localStorage.setItem("Recent Searches", JSON.stringify(lsSearches));
 
+  // Write Fetch Call to API
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${searchItem.value}&appid=${apiKey}`
   )
+    // Check out  data reurned from API
     .then(function (response) {
       return response.json();
     })
@@ -29,9 +30,10 @@ searchForm.addEventListener("submit", function (e) {
     .catch();
 });
 
+// Requires City name, returns Lat.Lon
 function getWeather(latitude, longitude, cityName, currentWeatherData) {
   console.log(latitude, longitude);
-
+  // Write Fetch Call to API
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
   )
@@ -44,13 +46,15 @@ function getWeather(latitude, longitude, cityName, currentWeatherData) {
 }
 
 function renderCurrentWeather(cityName, forecastWeather, currentWeatherData) {
+  // Todays Date
   var todayDate = dayjs().format("MM/DD/YYYY");
   console.log("current: ", currentWeatherData);
+
   console.log("forecast: ", forecastWeather.list);
   var tempKtoF = Math.floor(
     (currentWeatherData.main.temp - 273.15) * (9 / 5) + 32
   );
-
+  // Programatically display that data to the page
   currentWeatherEl.innerHTML = `
         <h3>${cityName}</h3>
                 <div>
@@ -65,15 +69,17 @@ function renderCurrentWeather(cityName, forecastWeather, currentWeatherData) {
 
   renderForecastWeather(forecastWeather);
 }
-
+// Create function for forecastWeather
 function renderForecastWeather(forecastWeather) {
   var elements = document.getElementsByClassName("card");
+  // for Loop for the forecast
   for (var i = 0; i < elements.length; i++) {
     x = i * 8;
     let date = dayjs()
       .add(i + 1, "day")
       .format("MM/DD/YYYY");
 
+    // Programatically display that data to the page
     elements[i].innerHTML = `
               <h3>${date}</h3>
               <p>Icon</p>
@@ -84,34 +90,4 @@ function renderForecastWeather(forecastWeather) {
   }
 }
 
-// Bring in DAYJS
 
-// Check out  data reurned from API
-// Requires Latitude and Long
-
-// Requires City name, returns Lat.Lon
-
-// Event Listener
-// Search Button
-// Each History Button
-
-// Function!!
-// Write Fetch Call to API
-// Find the informaiton that we want to display from the API call response
-// Todays Date
-// 5 day forcast
-
-// Render Function
-// Programatically display that data to the page
-// for Loop 2 times
-// Create x Card
-// Add x.header
-// Add x.image
-// Add x.temp
-// Add x.wind
-// Add x.humidity
-// Append x Card to some Div
-
-// Local Storage
-// When the page loads, grab all of the cities in LocalStorage
-// When a city name is input into the form and the search button in clicked, append the city to ecisting localStorage
